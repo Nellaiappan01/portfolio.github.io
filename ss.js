375d5b3b4c9cb92d8fb8ac1f6f62be2f90b498a9
NELLAIAPPAN S <108919574+Nellaiappan01@users.noreply.github.com>
2023-10-21 21:59:28 +0530

    Update ss.js

--- ss.js
+++ ss.js
@@ -14,6 +14,19 @@ trip.addEventListener('click',tripchange)
 function tripchange(){
     tripvisble.style.display = "block"
     kmvisble.style.display = "none"
-
-   
 }
+ const week2Radio = document.getElementById('week2');
+    const month2Radio = document.getElementById('month2');
+    
+    week2Radio.addEventListener('change', RadiocolorChange);
+    month2Radio.addEventListener('change', RadiocolorChange);
+    
+    function RadiocolorChange() {
+      if (week2Radio.checked) {
+        // Handle the "week2" radio button selected
+        document.body.style.backgroundColor = 'lightblue';
+      } else if (month2Radio.checked) {
+        // Handle the "month2" radio button selected
+        document.body.style.backgroundColor = 'lightgreen';
+      }
+    }
